9a59147f84a79d044c325a1293f26c8faaff5c5a
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-24 15:44:34 -0300

    feat: rendering tmpl pages with template.ParseFile + Execute

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
 
 const portNumber = ":8080"
 
 func Home(writer http.ResponseWriter, request *http.Request) {
-
+	renderTemplate(writer, "home.page")
 }
 
 func About(writer http.ResponseWriter, request *http.Request) {
+	renderTemplate(writer, "about.page")
+}
 
+func renderTemplate(writer http.ResponseWriter, name string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + name + ".tmpl")
+
+	err := parsedTemplate.Execute(writer, nil)
+
+	if err != nil {
+		log.Println("Error parsing page:", err)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
+	log.Printf(fmt.Sprintf("Starting server on port %s...\n", portNumber))
 	http.ListenAndServe(portNumber, nil)
 }
