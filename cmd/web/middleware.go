7f508940ac56bdc7e3ce8249ac15aa5414881e1f
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-26 21:27:12 -0300

    feat: creates a base cookie using nosurf and adds to middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
+// WriteToConsole is a custom middleware created just for testing
 func WriteToConsole(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hit the page!")
+		fmt.Println("Hit the page!")
 		next.ServeHTTP(w, r)
 	})
+}
+
+// NoSurf creates base cookie for the site using the nosurf lib
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,8 +15,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	// Middleware = functions that wrap your handlers to add common behavior before/after each request.
-	mux.Use(middleware.Logger, middleware.Recoverer)
-	mux.Use(WriteToConsole)
+	mux.Use(middleware.Logger, middleware.Recoverer, WriteToConsole, NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
