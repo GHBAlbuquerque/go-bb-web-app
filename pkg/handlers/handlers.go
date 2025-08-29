8776e47bbd40e913f1bed7a9ca3c19920508f299
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-29 14:05:06 -0300

    feat: implements getting information from the session on handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -23,12 +23,18 @@ func NewRepository(a *config.AppConfig) *Repository {
 }
 
 func (m *Repository) Home(writer http.ResponseWriter, request *http.Request) {
+	remoteIP := request.RemoteAddr
+	m.App.Session.Put(request.Context(), "remote_ip", remoteIP) // everytime somebody hits the homepage, i'm storing the ip
+
 	render.RenderTemplate(writer, "home.page.tmpl", &models.TemplateData{})
 }
 
 func (m *Repository) About(writer http.ResponseWriter, request *http.Request) {
 	stringMap := map[string]string{}
 	stringMap["test"] = "Hello again!"
+
+	remoteIP := m.App.Session.GetString(request.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(writer, "about.page.tmpl", &models.TemplateData{StringMap: stringMap})
 }
