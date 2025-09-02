a77f0ecd82a421201159843c5f80d81325045074
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-09-01 22:28:49 -0300

    feat: sending back a response in json format

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/GHBAlbuquerque/go-bb-web-app/pkg/config"
@@ -64,6 +67,31 @@ func (m *Repository) SearchAvailability(writer http.ResponseWriter, request *htt
 	render.RenderTemplate(writer, request, "search-availability.page.tmpl", &models.TemplateData{})
 }
 
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
 func (m *Repository) PostAvailability(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Posted to search-availability"))
+	start := request.Form.Get("start")
+	end := request.Form.Get("end")
+	adults := request.Form.Get("adults")
+	children := request.Form.Get("children")
+	room := request.Form.Get("room")
+
+	text := fmt.Sprintf("start date is %s, end date is %s, adults: %s, children: %s, room is %s", start, end, adults, children, room)
+	log.Println(text)
+
+	resp := jsonResponse{
+		OK:      true,
+		Message: text,
+	}
+
+	out, err := json.MarshalIndent(resp, "", "   ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(out)
 }
